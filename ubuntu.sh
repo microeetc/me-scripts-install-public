f8dabcb2ab3102df86b493ece448b0e79c62fdf1
Abel Ferreira <abelmferreira@gmail.com>
2024-12-22 02:30:40 -0300

    Add novos alias para comando ls

--- ubuntu.sh
+++ ubuntu.sh
@@ -74,6 +74,8 @@ cat << EOF >> ~/.bashrc
 alias ls='ls -FlhAv --color=auto'
 alias lt='ls -FlhAt --color=auto' 
 alias ltr='ls -FlhAtr --color=auto'
+alias cls="ls -lha --color=always -F --group-directories-first |awk '{k=0;s=0;for(i=0;i<=8;i++){;k+=((substr(\$1,i+2,1)~/[rwxst]/)*2^(8-i));};j=4;for(i=4;i<=10;i+=3){;s+=((substr(\$1,i,1)~/[stST]/)*j);j/=2;};if(k){;printf(\"%0o%0o \",s,k);};print;}'"
+alias lstree="ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'"
 
 export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\]\$ "
 export CLICOLOR=1
